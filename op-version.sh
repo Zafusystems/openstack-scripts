7739dcd3c3e2cbe8bf8a115071b58378ee08aad6
Zafu Systems <ops@zafu.systems>
2018-05-15 14:23:30 +0200

    op-version

---
+++ op-version.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#
+# Simple script which prints Openstack version
+# 
+# 15.5.2018, ops@zafu.systems
+#
+# Usage: ./op-version.sh
+#
+
+SOURCES_FILE="/etc/apt/sources.list.d/ubuntu-cloud.list"
+
+OP_VERSION="$(awk -F'/' '/deb/ { print $5 }' "$SOURCES_FILE" | sed 's/main//')"
+
+echo "Openstack version: ${OP_VERSION^}"
